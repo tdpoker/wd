855c3f962a03616b9926a09c3356aa507684aaa5
tdpoker <31983686+tdpoker@users.noreply.github.com>
2020-11-20 23:56:30 +0700

    Update wd.js

--- wd.js
+++ wd.js
@@ -1,3 +1,4 @@
+if(window.location.hostname == 'winwithiceland.com' ){
 $(document).ready(function(){
 });
     $('#wdpoker1').html('<h1>Judi Online | Kartu Poker | Dominoqq | Bandarq | Capsa Susun - WdPoker</h1> <a href="/">Wdpoker</a> adalah situs judi online dengan server terbaru. Main kartu poker online dengan 13 jenis yang bisa anda coba. Dewa dari segala permainan kartu remi. Tidak hanya itu, kami juga menyediakan dominoqq, bandar ceme, capsa susun.<br><h2>Judi Kartu Poker Online Bersama WdPoker</h2> judi poker yang bisa dimainkan ada 7 yang bisa kalian coba. Poker biasa, enam plus, bandar, AoF, AoF6+, omaha, Pot adalah ke tujuh game yang bisa kalian coba. Berbeda dari permainan yang disediakan oleh pokerv dan juga idnpoker. Jadi jangan tunggu lagi untuk bergabung bersama dengan kami disini.<br><h2>Situs Judi Dominoqq Terpercaya di Indonesia</h2> Situs judi dominoqq terpercaya yang bisa kalian coba bersama dengan kami. Gabung dengan ribuan pemain dalam 1 meja diseluruh indonesia.<br><h2>Raja Bandar Ceme Terbaik di Wdpoker</h2> 3 bandar ceme yang bisa anda mainkan yaitu bandar ceme, ceme keliling dan juga ceme biasa. Daftar sekarang juga untuk main bandar ceme online bersama dengan ribuan pemain diseluruh Indonesia.<br><h2>Capsa Susun | Capsun | Capsa | WdPoker</h2> Capsa susun online yang sering dikenal dengan sebutan capsa atau capsun ini populer di Indonesia. Permainan unik dengan menyusun 13 kartu untuk mematahkan kombinasi kartu lawan.<br>');
@@ -6,7 +7,7 @@ $(document).ready(function(){
             $(document).ready(function(){
             document.title = 'Judi Online | Kartu Poker | Dominoqq | Bandarq | Capsa Susun - WdPoker';
             $("meta[name='description']").attr('content', 'Situs judi online dengan kartu poker. Tersedia 12 jenis poker, dominoqq, bandarq dan capsa susun. Daftar sekarang juga di Wdpoker.');
-            $("link[rel='canonical']").attr('href', 'http://128.199.101.233/desktop/home');
+            $("link[rel='canonical']").attr('href', 'http://winwithiceland.com/desktop/home');
             $('head').append($('<link rel="alternate" media="only screen and (max-width: 640px)" />').attr('href', '/mobile/home'));
         });
     }
@@ -14,7 +15,7 @@ $(document).ready(function(){
         $(document).ready(function(){
             document.title = 'Daftar Tournament Poker Terbesar di Indonesia dengan Hadiah Miliaran Rupiah.';
             $("meta[name='description']").attr('content', 'Daftar dan gabung tournament poker di Indonesia dan menangkan hadiah miliaran rupiah bersama wdpoker.');
-            $("link[rel='canonical']").attr('href', 'https://wdpoker.com/desktop/tournament');
+            $("link[rel='canonical']").attr('href', 'http://winwithiceland.com/desktop/tournament');
             $('head').append($('<link rel="alternate" media="only screen and (max-width: 640px)" />').attr('href', '/mobile/tournament'));
         });
     }
@@ -22,7 +23,7 @@ $(document).ready(function(){
         $(document).ready(function(){
             document.title = 'Menang Jackpot Poker dengan Mudah - Wdpoker';
             $("meta[name='description']").attr('content', 'Menang jackpot sekarang lebih mudah bersama dengan game terbaru kami di wdpoker');
-            $("link[rel='canonical']").attr('href', 'https://wdpoker.com/desktop/jackpot');
+            $("link[rel='canonical']").attr('href', 'http://winwithiceland.com/desktop/jackpot');
             $('head').append($('<link rel="alternate" media="only screen and (max-width: 640px)" />').attr('href', '/mobile/jackpot'));
         });
     }
@@ -30,7 +31,7 @@ $(document).ready(function(){
         $(document).ready(function(){
             document.title = 'Promo Terbaru | Info Freechip | Freebet | Gratis | Event';
             $("meta[name='description']").attr('content', 'Jangan sampai ketinggalan info freebet, freechip, event dan juga promo terbaru dari wdpoker');
-            $("link[rel='canonical']").attr('href', 'https://wdpoker.com/desktop/promotion');
+            $("link[rel='canonical']").attr('href', 'http://winwithiceland.com/desktop/promotion');
             $('head').append($('<link rel="alternate" media="only screen and (max-width: 640px)" />').attr('href', '/mobile/promotion'));
 
         });
@@ -39,42 +40,42 @@ $(document).ready(function(){
         $(document).ready(function(){
             document.title = 'Install Aplikasi Poker Online di Android';
             $("meta[name='description']").attr('content', 'Ikuti cara mudah install aplikasi poker online mudah di android lengkap dengan gambar dan tutorial.');
-            $("link[rel='canonical']").attr('href', 'https://wdpoker.com/desktop/tutorial/install-android');
+            $("link[rel='canonical']").attr('href', 'http://winwithiceland.com/desktop/tutorial/install-android');
         });
     }
     else if(document.URL.indexOf('faq')>0){
         $(document).ready(function(){
             document.title = 'Pertanyaan | FAQ | Tata Cara | Panduan | Syarat dan Ketentuan';
             $("meta[name='description']").attr('content', 'Panduan dan bacaan untuk mendaftar di situs wdpoker. Faq yang sering ditanyakan oleh pemain.');
-            $("link[rel='canonical']").attr('href', 'https://wdpoker.com/desktop/faq');
+            $("link[rel='canonical']").attr('href', 'http://winwithiceland.com/desktop/faq');
         });
     }
     else if(document.URL.indexOf('terms-of-use')>0){
         $(document).ready(function(){
             document.title = 'Terms of Use | WdPoker';
             $("meta[name='description']").attr('content', 'Syarat dan ketentuan dalam menggunakan situs wdpoker.');
-            $("link[rel='canonical']").attr('href', 'https://wdpoker.com/desktop/terms-of-use');
+            $("link[rel='canonical']").attr('href', 'http://winwithiceland.com/desktop/terms-of-use');
         });
     }
     else if(document.URL.indexOf('responsible-gaming')>0){
         $(document).ready(function(){
             document.title = 'Responsible Gaming | WdPoker';
             $("meta[name='description']").attr('content', 'Ikuti saran dalam memainkan sebuah game agar tidak mengganggu kehidupan anda.');
-            $("link[rel='canonical']").attr('href', 'https://wdpoker.com/desktop/responsible-gaming');
+            $("link[rel='canonical']").attr('href', 'http://winwithiceland.com/desktop/responsible-gaming');
         });
     }
     else if(document.URL.indexOf('jackpot-bandar-poker')>0){
         $(document).ready(function(){
             document.title = 'Jackpot Bandar Poker';
             $("meta[name='description']").attr('content', 'Panduan beli jackpot di game bandar poker bersama situs wdpoker.');
-            $("link[rel='canonical']").attr('href', 'https://wdpoker.com/desktop/jackpot-bandar-poker');
+            $("link[rel='canonical']").attr('href', 'http://winwithiceland.com/desktop/jackpot-bandar-poker');
         });
     }
     else if(document.URL.indexOf('jackpot-poker-omaha')>0){
         $(document).ready(function(){
             document.title = 'Jackpot Bandar omaha';
             $("meta[name='description']").attr('content', 'Panduan beli jackpot di game bandar omaha bersama situs wdpoker.');
-            $("link[rel='canonical']").attr('href', 'https://wdpoker.com/desktop/jackpot-bandar-omaha');
+            $("link[rel='canonical']").attr('href', 'http://winwithiceland.com/desktop/jackpot-bandar-omaha');
         });
     }
     else {
@@ -85,12 +86,92 @@ $(document).ready(function(){
     }
     
 }
-else if(document.URL.indexOf('mobile')>0){
-    if(document.URL.indexOf('home')>0){
-        $(document).ready(function(){
-        document.title = 'Judi Online | Kartu Poker | Dominoqq | Bandarq | Capsa Susun - WdPoker';
-        $("meta[name='description']").attr('content', 'Situs judi online dengan kartu poker. Tersedia 12 jenis poker, dominoqq, bandarq dan capsa susun. Daftar sekarang juga di Wdpoker.');
-        $("link[rel='canonical']").attr('href', 'http://128.199.101.233/desktop/home');
+}
+else if(window.location.hostname == '128.199.101.233' ){
+    $(document).ready(function(){
     });
-}
-}
+        if(document.URL.indexOf('desktop')>0){
+            if(document.URL.indexOf('home')>0){
+                $(document).ready(function(){
+                document.title = 'WdPoker - Situs Poker | Ludo King | QQ Online | Dadu Online';
+                $("meta[name='description']").attr('content', 'Wdpoker merupakan situs poker dengan permainan kartu, ludo king, domino qq, dadu online dan masih banyak lagi permainan yang bisa dimainkan.');
+                $("link[rel='canonical']").attr('href', 'http://128.199.101.233/desktop/home');
+                $('head').append($('<link rel="alternate" media="only screen and (max-width: 640px)" />').attr('href', '/mobile/home'));
+            });
+        }
+        else if(document.URL.indexOf('tournament')>0){
+            $(document).ready(function(){
+                document.title = 'Daftar Turnamen Poker Terbesar di Indonesia Gratis - WdPoker';
+                $("meta[name='description']").attr('content', 'Gabung bersama ribuan pemain poker di Indonesia dengan mengikuti turnamen terbesar dengan hadiah sampai dengan miliaran rupiah.');
+                $("link[rel='canonical']").attr('href', 'http://128.199.101.233/desktop/tournament');
+                $('head').append($('<link rel="alternate" media="only screen and (max-width: 640px)" />').attr('href', '/mobile/tournament'));
+            });
+        }
+        else if(document.URL.indexOf('jackpot')>0){
+            $(document).ready(function(){
+                document.title = 'Cara Beli Jackpot, Menang Jackpot dan Perkalian Hadiah';
+                $("meta[name='description']").attr('content', 'Cara untuk beli jackpot dengan mengikuti panduan yang kami berikan. Menangkan hadiah jackpot dengan perkalian lebih dari 100x pembelian jackpot.');
+                $("link[rel='canonical']").attr('href', 'http://128.199.101.233/desktop/jackpot');
+                $('head').append($('<link rel="alternate" media="only screen and (max-width: 640px)" />').attr('href', '/mobile/jackpot'));
+            });
+        }
+        else if(document.URL.indexOf('promotion')>0){
+            $(document).ready(function(){
+                document.title = 'Info Freebet | Promo Gratis | Bonus Chip Poker - WdPoker';
+                $("meta[name='description']").attr('content', 'Informasi freebet, promo gratis dan juga bonus chip poker untuk situs wdpoker. Kami memberikan berbagai hadiah kepada para pemain yang sering main judi online bersama wdpoker.');
+                $("link[rel='canonical']").attr('href', 'http://128.199.101.233/desktop/promotion');
+                $('head').append($('<link rel="alternate" media="only screen and (max-width: 640px)" />').attr('href', '/mobile/promotion'));
+    
+            });
+        }
+        else if(document.URL.indexOf('install-android')>0){
+            $(document).ready(function(){
+                document.title = 'Install Aplikasi Untuk Main Langsung di Android dan Ios.';
+                $("meta[name='description']").attr('content', 'Ikuti Panduan untuk instalasi aplikasi poker online di android dan juga Ios. Semakin mudah dan gampang untuk mengakses situs judi online.');
+                $("link[rel='canonical']").attr('href', 'http://128.199.101.233/desktop/tutorial/install-android');
+            });
+        }
+        else if(document.URL.indexOf('faq')>0){
+            $(document).ready(function(){
+                document.title = 'Pertanyaan | Tanya-Tanya | Butuh Bantuan';
+                $("meta[name='description']").attr('content', 'Jika anda memiliki pertanyaan dan juga ingin tanya-tanya serta butuh bantuan dalam menggunakan situs wdpoker, bisa membaca dan juga melihat apa yang sering ditanyakan oleh para pengguna.');
+                $("link[rel='canonical']").attr('href', 'http://128.199.101.233/desktop/faq');
+            });
+        }
+        else if(document.URL.indexOf('terms-of-use')>0){
+            $(document).ready(function(){
+                document.title = 'Penggunaan Situs - WdPoker';
+                $("meta[name='description']").attr('content', 'Syarat yang harus kalian ketahui untuk menggunakan situs wdpoker. Ketentuan penggunaan layanan kami juga bisa dibaca di sini.');
+                $("link[rel='canonical']").attr('href', 'http://128.199.101.233/desktop/terms-of-use');
+            });
+        }
+        else if(document.URL.indexOf('responsible-gaming')>0){
+            $(document).ready(function(){
+                document.title = 'Responsible Gaming | WdPoker';
+                $("meta[name='description']").attr('content', 'Ikuti saran dalam memainkan sebuah game agar tidak mengganggu kehidupan anda.');
+                $("link[rel='canonical']").attr('href', 'http://128.199.101.233/desktop/responsible-gaming');
+            });
+        }
+        else if(document.URL.indexOf('jackpot-bandar-poker')>0){
+            $(document).ready(function(){
+                document.title = 'Jackpot Bandar Poker';
+                $("meta[name='description']").attr('content', 'Panduan beli jackpot di game bandar poker bersama situs wdpoker.');
+                $("link[rel='canonical']").attr('href', 'http://128.199.101.233/desktop/jackpot-bandar-poker');
+            });
+        }
+        else if(document.URL.indexOf('jackpot-poker-omaha')>0){
+            $(document).ready(function(){
+                document.title = 'Jackpot Bandar omaha';
+                $("meta[name='description']").attr('content', 'Panduan beli jackpot di game bandar omaha bersama situs wdpoker.');
+                $("link[rel='canonical']").attr('href', 'http://128.199.101.233/desktop/jackpot-bandar-omaha');
+            });
+        }
+        else {
+            $(document).ready(function(){
+                document.title = 'Cara Main Poker | Beli Jackpot | Event | Tournament';
+                $("link[rel='canonical']").attr('href', '/');
+            });
+        }
+        
+    }
+    }
